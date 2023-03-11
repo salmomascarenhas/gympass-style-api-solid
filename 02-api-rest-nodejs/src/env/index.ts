dc2891d22bead0611a6576920098e20f689769c5
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-11 13:30:41 -0300

    refactoring the usability of dotnev

---
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+    DATABASE_URL: z.string(),
+    PORT: z.number().default(3333)
+})
+
+export const env = envSchema.parse(process.env)
--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,7 +12,7 @@ app.get('/hello', async () => {
 
 app
     .listen({
-        port: 3333,
+        port: env.PORT,
     })
     .then(() => {
         console.log('HTTP Server Running!')
