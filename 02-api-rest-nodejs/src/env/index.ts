336e7ff63bd3fedcb0e98e59b969a51a798df326
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-11 13:38:19 -0300

    Create verification of dotenv

--- 02-api-rest-nodejs/src/env/index.ts
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -7,4 +7,12 @@ const envSchema = z.object({
     PORT: z.number().default(3333)
 })
 
-export const env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+    console.error('⚠️ Invalid environment variables!', _env.error.format())
+
+    throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
