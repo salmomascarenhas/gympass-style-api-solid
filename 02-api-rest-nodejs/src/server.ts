c77485bb76f6db1fd47f727e633ddf89e2e8a16e
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-11 18:46:42 -0300

    savig sessoin_id when user does a request

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
-
+import cookie from '@fastify/cookie'
 const app = fastify()
 
-app.register(transactionsRoutes, { prefix: 'transactions' })
+app.register(cookie)
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
+})
 
 app
     .listen({
