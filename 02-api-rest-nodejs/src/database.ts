3f38b62669bd8db20e1807a64a4890572fc9b9ea
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-12 19:58:04 -0300

    update dotenv example

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -2,10 +2,10 @@ import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-    client: 'sqlite',
-    connection: {
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_URL === 'sqlite' ? {
         filename: env.DATABASE_URL
-    },
+    } : env.PG_CONNECTION_STRING,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
