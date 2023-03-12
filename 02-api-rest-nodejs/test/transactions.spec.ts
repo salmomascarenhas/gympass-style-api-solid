869af062c858f5486852109eea5d00cbd2bce457
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-11 22:38:02 -0300

    refact agroupment and use it method

--- 02-api-rest-nodejs/test/transactions.spec.ts
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,21 +1,23 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-    await app.ready()
-})
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
 
-afterAll(async () => {
-    await app.close
-})
+    afterAll(async () => {
+        await app.close
+    })
 
-test('User can create a new transaction', async () => {
-    await request(app.server)
-        .post('/transactions').send({
-            title: 'New Transaction',
-            amount: 5000,
-            type: 'credit'
-        })
-        .expect(201)
+    it('Sould be able to user create a new transaction', async () => {
+        await request(app.server)
+            .post('/transactions').send({
+                title: 'New Transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+            .expect(201)
+    })
 })
