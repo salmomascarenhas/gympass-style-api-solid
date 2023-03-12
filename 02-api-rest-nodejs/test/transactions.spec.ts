3db9f5ff265ae3ecd6ae40a1db6ab27035db596f
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-11 22:58:18 -0300

    ensure Transactions should be able to list all transactions

--- 02-api-rest-nodejs/test/transactions.spec.ts
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -19,5 +19,29 @@ describe('Transactions routes', () => {
                 type: 'credit'
             })
             .expect(201)
+    })
+
+    it('Should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions').send({
+                title: 'New Transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200)
+
+        expect(listTransactionResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New Transaction',
+                amount: 5000
+            })
+        ])
+
     })
 })
