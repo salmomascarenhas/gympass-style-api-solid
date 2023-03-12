81b0b0cd64bc00cdaec08ae540f023b5121169cd
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-11 22:32:50 -0300

    Ensure Transactions that the user cant creatae a new transaction

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,16 +1,5 @@
-import fastify from 'fastify'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-const app = fastify()
-
-app.register(cookie)
-app.addHook('preHandler', async (request) => {
-    console.info(`[${request.method}] ${request.url}`)
-})
-app.register(transactionsRoutes, {
-    prefix: 'transactions'
-})
+import { app } from './app'
 
 app
     .listen({
