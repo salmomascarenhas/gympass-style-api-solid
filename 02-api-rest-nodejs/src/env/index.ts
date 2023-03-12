dae51a5df281f8fcd28fe4a52ee71d3f3b7e5a45
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-12 19:58:25 -0300

    update type dotenv

--- 02-api-rest-nodejs/src/env/index.ts
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -10,7 +10,9 @@ if (process.env.NODE_ENV === 'test') {
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
     DATABASE_URL: z.string(),
-    PORT: z.number().default(3333)
+    DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+    PG_CONNECTION_STRING: z.string(),
+    PORT: z.coerce.number().default(3333)
 })
 
 const _env = envSchema.safeParse(process.env)
