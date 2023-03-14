569b9ee6587b38c9d097f09e1ef8b09cb33f6e67
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-14 19:44:23 -0300

    eslint formatter

--- 03-api-solid/src/env/index.ts
+++ 03-api-solid/src/env/index.ts
@@ -1,16 +1,16 @@
-import { config } from 'dotenv'
+import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
-    PORT: z.coerce.number().default(3333)
+    PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
     console.error('❌ Invalid enviroment variables', _env.error.format())
-    throw new Error('Invalid environment variable')
+    throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,9 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({
+app
+  .listen({
     port: env.PORT,
-    host: '0.0.0.0'
-}).then(() => {
+    host: '0.0.0.0',
+  })
+  .then(() => {
     console.log('🚀 HTTP Server Is Runing!')
-})
+  })
