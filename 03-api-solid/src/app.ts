e5866f4c983e7a9ed56fbfcb9351a493949084eb
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-15 20:46:38 -0300

    route create user

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,3 +1,26 @@
 import fastfy from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastfy()
+
+
+app.post('/users', async (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    })
+
+    return reply.status(201).send()
+})
