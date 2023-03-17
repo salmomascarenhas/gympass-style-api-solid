b2f7103f199ba4585d9e759f83c5286f33c90dd1
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-17 12:10:43 -0300

    implements finById method from interface UsersRepository.

--- 03-api-solid/src/repositories/in-memory/in-memory-users.repository.ts
+++ 03-api-solid/src/repositories/in-memory/in-memory-users.repository.ts
@@ -3,6 +3,16 @@ import { User, Prisma } from '@prisma/client'
 export class InMemoryUsersRepository implements UsersRepository {
     public items: User[] = []
 
+    async findById(id: string) {
+        const user = this.items.find((item) => item.id === id)
+
+        console.log(user)
+        if (!user)
+            return null
+
+        return user
+    }
+
     async findByEmail(email: string) {
         const user = this.items.find((item) => item.email === email)
 
--- 03-api-solid/src/repositories/prisma/prisma-users.repository.ts
+++ 03-api-solid/src/repositories/prisma/prisma-users.repository.ts
@@ -2,12 +2,13 @@ import { prisma } from '@/lib/prisma'
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users.repository'
 export class PrismaUsersRepository implements UsersRepository {
-    async create(data: Prisma.UserCreateInput) {
-        const user = await prisma.user.create({
-            data
-        })
 
-        return user
+    async findById(id: string): Promise<User | null> {
+        return await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
     }
 
     async findByEmail(email: string): Promise<User | null> {
@@ -18,4 +19,11 @@ export class PrismaUsersRepository implements UsersRepository {
         })
     }
 
+    async create(data: Prisma.UserCreateInput) {
+        const user = await prisma.user.create({
+            data
+        })
+
+        return user
+    }
 }
