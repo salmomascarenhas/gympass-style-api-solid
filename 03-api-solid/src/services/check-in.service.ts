c11586fd8fbe80b589ce86136955f20314513bb7
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-17 21:36:03 -0300

    add if checkIn is on same day

--- 03-api-solid/src/services/check-in.service.ts
+++ 03-api-solid/src/services/check-in.service.ts
@@ -16,6 +16,12 @@ export class CheckInService {
     async execute({
         userId,
         gymId }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+
+        if (checkInOnSameDay) {
+            throw new Error()
+        }
+
         const checkIn = await this.checkInsRepository.create({
             user_id: userId,
             gym_id: gymId
