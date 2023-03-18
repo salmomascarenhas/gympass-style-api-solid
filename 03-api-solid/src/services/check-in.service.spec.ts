5754392a8a0e4154f79d0f82ac179624798fa763
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-17 21:35:29 -0300

    mock time in tests

--- 03-api-solid/src/services/check-in.service.spec.ts
+++ 03-api-solid/src/services/check-in.service.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInsRepository } from '../repositories/check-ins.repository'
 import { CheckInService } from './check-in.service'
 import { InMemoryCheckInsRepository } from '../repositories/in-memory/in-memory-check-ins.repository'
@@ -10,9 +10,15 @@ describe('CheckIn Service', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new CheckInService(checkInsRepository)
+
+        vi.useFakeTimers()
     })
 
-    it('Should be able create a check in', async () => {
+    afterEach(() => {
+        vi.useRealTimers()
+    })
+
+    it('Should be able to check in', async () => {
 
         const { checkIn } = await sut.execute({
             gymId: 'gym-id',
@@ -20,6 +26,39 @@ describe('CheckIn Service', () => {
         })
 
         expect(checkIn.gym_id).toEqual(expect.any(String))
+    })
+
+    it('Should not be able to check in twice in the same day ', async () => {
+        vi.setSystemTime(new Date(2022, 5, 20, 8, 0, 0))
+
+        await sut.execute({
+            gymId: 'gym-id',
+            userId: 'user-id'
+        })
+
+        await expect(() =>
+            sut.execute({
+                gymId: 'gym-id',
+                userId: 'user-id'
+            })).rejects.toBeInstanceOf(Error)
+    })
+
+    it('Should be able to check in twice but in the different days ', async () => {
+        vi.setSystemTime(new Date(2022, 5, 20, 8, 0, 0))
+
+        await sut.execute({
+            gymId: 'gym-id',
+            userId: 'user-id'
+        })
+
+        vi.setSystemTime(new Date(2022, 5, 21, 8, 0, 0))
+
+        const { checkIn } = await sut.execute({
+            gymId: 'gym-id',
+            userId: 'user-id'
+        })
+
+        expect(checkIn.id).toEqual(expect.any(String))
     })
 
 })
