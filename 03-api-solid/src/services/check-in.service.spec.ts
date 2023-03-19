6a719ed0b9957ef72f5fa2093a475620bee0cad1
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-19 12:38:08 -0300

    use create method of gyms

--- 03-api-solid/src/services/check-in.service.spec.ts
+++ 03-api-solid/src/services/check-in.service.spec.ts
@@ -1,29 +1,28 @@
 import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInService } from './check-in.service'
 import { InMemoryCheckInsRepository } from '../repositories/in-memory/in-memory-check-ins.repository'
-import { InMemoryGymsRepository } from '../repositories/in-memory-check-ins.repository'
-import { Decimal } from '@prisma/client/runtime/library'
+import { InMemoryGymsRepository } from '../repositories/in-memory/in-memory-gyms.repository'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let gymsRepository: InMemoryGymsRepository
 let sut: CheckInService
 
 describe('CheckIn Service', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
         checkInsRepository = new InMemoryCheckInsRepository()
         gymsRepository = new InMemoryGymsRepository()
 
         sut = new CheckInService(checkInsRepository, gymsRepository)
 
-        gymsRepository.items.push({
+        await gymsRepository.create({
             id: 'gym-id',
             title: ' JavaScript Gym',
             description: '',
             phone: '',
-            latitude: new Decimal(-3.7358774),
-            longitude: new Decimal(-41.0091473),
-            created_at: new Date()
-
+            latitude: -3.7358774,
+            longitude: -41.0091473,
         })
 
         vi.useFakeTimers()
@@ -61,7 +60,7 @@ describe('CheckIn Service', () => {
                 userId: 'user-id',
                 userLatitude: -3.7358774,
                 userLongitude: -41.0091473,
-            })).rejects.toBeInstanceOf(Error)
+            })).rejects.toBeInstanceOf(MaxNumberOfCheckInsError)
     })
 
     it('Should be able to check in twice but in the different days ', async () => {
@@ -87,25 +86,22 @@ describe('CheckIn Service', () => {
     })
 
     it('Should not be able to check in on distant gym', async () => {
-        gymsRepository.items.push({
+        await gymsRepository.create({
             id: 'gym-id-01',
-            title: ' JavaScript Gym',
+            title: 'JavaScript Gym',
             description: '',
             phone: '',
-            latitude: new Decimal(-3.6777296081087285),
-            longitude: new Decimal(-40.968637630135966),
-            created_at: new Date()
-
-
+            latitude: -3.6777296081087285,
+            longitude: -40.968637630135966,
         })
 
         await expect(() =>
             sut.execute({
                 gymId: 'gym-id-01',
-                userId: 'user-id',
+                userId: 'JavaScript Gym',
                 userLatitude: -3.7358774,
                 userLongitude: -41.0091473,
-            })).rejects.toBeInstanceOf(Error)
+            })).rejects.toBeInstanceOf(MaxDistanceError)
 
 
     })
