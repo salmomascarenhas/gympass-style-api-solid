a7b7f19460370c1a6d8fde85806df510cd33d1c5
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-19 23:20:07 -0300

    implements save and bindById methods

--- 03-api-solid/src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ 03-api-solid/src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -3,7 +3,30 @@ import { CheckInsRepository } from '../check-ins.repository'
 import { randomUUID } from 'node:crypto'
 import dayjs from 'dayjs'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+
     public items: CheckIn[] = []
+
+    async save(checkIn: CheckIn): Promise<CheckIn> {
+        const checkInIndex = this.items.findIndex(item => item.id === checkIn.id)
+
+        if (checkInIndex === -1) {
+            throw new Error('Check-in not found')
+        }
+
+        this.items[checkInIndex] = checkIn
+
+        return checkIn
+    }
+
+    async findById(id: string): Promise<CheckIn | null> {
+        const checkIn = this.items.find(item => item.id === id)
+
+        if (!checkIn) {
+            return null
+        }
+
+        return checkIn
+    }
 
     async getUserMetrics(userId: string): Promise<number> {
         return this.items.filter(item => item.user_id === userId).length
