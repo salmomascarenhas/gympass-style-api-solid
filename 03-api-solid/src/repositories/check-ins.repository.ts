aa811e4108ecf3dfb49ba72d8315198a88dd427d
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-03-26 21:50:44 -0300

    create countByUserId

--- 03-api-solid/src/repositories/check-ins.repository.ts
+++ 03-api-solid/src/repositories/check-ins.repository.ts
@@ -7,4 +7,6 @@ export interface CheckInsRepository {
     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+    countByUserId(userId: string): Promise<number>
+
 }
