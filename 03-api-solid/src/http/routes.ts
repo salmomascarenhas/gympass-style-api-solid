5a15b1e5403dd6509575ab2b0f2a378b78654a9d
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-02 16:14:39 -0300

    create new route /me

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from "fastify"
 import { registerController } from "./controllers/register.controller"
 import { authenticateController } from "./controllers/authenticate.controller"
+import { profileController } from "./controllers/profile.controller"
+import { verifyJwt } from "./middlewares/verify-jwt"
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', registerController)
     app.post('/sessions', authenticateController)
+
+    /**  Authenticated routes */
+    app.get('/me', { onRequest: [verifyJwt] }, profileController)
 
 }
