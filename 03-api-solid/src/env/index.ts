968ccce404f5ba19b7268269f5e090c76534687a
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-02 16:14:20 -0300

    update type with JWT_TOKEN

--- 03-api-solid/src/env/index.ts
+++ 03-api-solid/src/env/index.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
     PORT: z.coerce.number().default(3333),
+    JWT_SECRET: z.string().nonempty(),
 })
 
 const _env = envSchema.safeParse(process.env)
