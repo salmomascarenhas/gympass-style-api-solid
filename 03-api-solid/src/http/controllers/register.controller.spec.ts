b2c044d06c2bc650ddf0dd64d6e7e4b2a6755372
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-13 11:58:14 -0300

    Ensure register (e2e) should be able to register

--- 03-api-solid/src/http/controllers/register.controller.spec.ts
+++ 03-api-solid/src/http/controllers/register.controller.spec.ts
@@ -1,5 +1,26 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {
-    console.log('dentro do test')
+describe('Register (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to register', async () => {
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name: 'Salmo',
+                email: 'salmo.cruz@gmail.com',
+                password: '123456'
+            })
+
+        expect(response.statusCode).toBe(201)
+    })
+
 })
