e1c21f1fee1e69858ebda23f0689c16657f76bcd
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-13 20:11:34 -0300

    update error

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,15 @@
 import 'dotenv/config'
+
 import { randomUUID } from 'node:crypto'
-import { Environment } from 'vitest'
 import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-function generateDatabaseUrl(schema: string) {
+function generateDatabaseURL(schema: string) {
     if (!process.env.DATABASE_URL) {
-        throw new Error('DATABASE_URL is not defined')
+        throw new Error('Please provide a DATABASE_URL environment variable.')
     }
 
     const url = new URL(process.env.DATABASE_URL)
@@ -22,22 +23,20 @@ export default <Environment>{
     name: 'prisma',
     async setup() {
         const schema = randomUUID()
-        const databaseUrl = generateDatabaseUrl(schema)
+        const databaseURL = generateDatabaseURL(schema)
 
-        process.env.DATABASE_URL = databaseUrl
+        process.env.DATABASE_URL = databaseURL
 
-        console.log(`Using database ${process.env.DATABASE_URL}`)
-
-        execSync(`npx prisma migrate deploy`)
+        execSync('npx prisma migrate deploy')
 
         return {
             async teardown() {
                 await prisma.$executeRawUnsafe(
-                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`
+                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
                 )
 
                 await prisma.$disconnect()
-            }
+            },
         }
-    }
+    },
 }
