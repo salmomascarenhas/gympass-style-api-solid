18ce9aabc206ea5f326f22a42af7f1fa12dc0d27
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-13 21:15:55 -0300

    remove done param

--- src/http/middlewares/verify-jwt.ts
+++ src/http/middlewares/verify-jwt.ts
@@ -1,11 +1,9 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
 
-export async function verifyJwt(request: FastifyRequest, reply: FastifyReply, done: (err?: Error) => void) {
+export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
     try {
         await request.jwtVerify()
-    } catch (error) {
-        reply.status(401).send({
-            message: 'Unauthorized'
-        })
+    } catch (err) {
+        return reply.status(401).send({ message: 'Unauthorized.' })
     }
 }
