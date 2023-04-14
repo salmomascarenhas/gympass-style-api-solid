a7c9aca4ded4dde7b22faf32d7647998cc3203c9
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-14 10:08:09 -0300

    feature: Create controller and use case for refresh token.

---
+++ src/http/controllers/users/refresh.controller.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function refreshController(request: FastifyRequest, reply: FastifyReply) {
+    await request.jwtVerify({ onlyCookie: true })
+
+    const token = await reply.jwtSign(
+        {},
+        {
+            sign: {
+                sub: request.user.sub,
+            }
+        })
+
+    const refreshToken = await reply.jwtSign(
+        {},
+        {
+            sign: {
+                sub: request.user.sub,
+                expiresIn: '7d'
+            }
+        })
+
+    return reply
+        .setCookie('refreshToken', refreshToken, {
+            path: '/',
+            secure: true,
+            sameSite: true,
+            httpOnly: true,
+        })
+        .status(200)
+        .send({
+            token
+        })
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,11 +4,14 @@ import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { registerController } from './register.controller'
 import { authenticateController } from './authenticate.controller'
 import { profileController } from './profile.controller'
+import { refreshController } from './refresh.controller'
 
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/users', registerController)
     app.post('/sessions', authenticateController)
+
+    app.patch('/token/refresh', refreshController)
 
     /** Authenticated */
     app.get('/me', { onRequest: [verifyJwt] }, profileController)
