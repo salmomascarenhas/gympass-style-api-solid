1b58488bc181d8be059783f485f92e0824f25a52
Salmo Mascarenhas <salmo.cruz@gmail.com>
2023-04-14 09:56:16 -0300

    feature: Generate refreshToken through secure cookie

--- src/http/controllers/users/authenticate.controller.ts
+++ src/http/controllers/users/authenticate.controller.ts
@@ -23,9 +23,26 @@ export async function authenticateController(request: FastifyRequest, reply: Fas
                 }
             })
 
-        return reply.status(200).send({
-            token
-        })
+        const refreshToken = await reply.jwtSign(
+            {},
+            {
+                sign: {
+                    sub: user.id,
+                    expiresIn: '7d'
+                }
+            })
+
+        return reply
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .status(200)
+            .send({
+                token
+            })
 
     } catch (error) {
         if (error instanceof InvalidCredentialsError) {
